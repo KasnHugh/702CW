f2cd84b11742ace7cf466fb33c871e53b359a111
Kasper Groes Ludvigsen <groes.ludvigsen@gmail.com>
2020-11-06 00:15:05 +0100

    The function now returns simplified data that is split into training and testing data for X and y respectively

--- load_mnist_data.py
+++ load_mnist_data.py
@@ -4,24 +4,47 @@ Created on Wed Nov  4 17:55:26 2020
 
 @author: groes
 """
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import fetch_openml
 
-def load_data(path):
+def load_and_split_data(test_size):
     '''
+    
+
     Parameters
     ----------
-    path : str
-        Absolute path to the folder you either want to store the dataset or already have it
-        
+    test_size : float
+        the proportion of the dataset you want to use as test data, e.g. 0.25
+
     Returns
     -------
-    mnist_dataset.train_data, mnist_dataset.test_data : mnist objects
-        the training and testing data of the mnist dataset
+    X_train : pandas series
+        input features split into training data
+    X_test : pandas series
+        input features split into test data
+    y_train : pandas series
+        target features split into training data
+    y_test : pandas series
+        target features split into test data
 
     '''
-    import torchvision.datasets as dset
-    mnist_dataset = dset.MNIST(
-        root = path,
-        download = True
-        )
+    # The next line is borrowed from here:
+    # https://github.com/ageron/handson-ml/issues/301
+    mnist = fetch_openml('mnist_784', version=1, cache=True)
     
-    return mnist_dataset.train_data, mnist_dataset.test_data
+    df = pd.DataFrame(data=np.c_[mnist['data'],mnist['target']],
+                  columns=np.append(mnist['feature_names'],['target']))
+    X = df.drop("target", axis=1)
+    y = df.target
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=42
+        ) 
+    
+    return X_train, X_test, y_train, y_test
+
+
+
+
+
