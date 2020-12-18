405278dd45d1d9c59c4cb287d6b614cc9cb433de
HughAdams1 <73829484+HughAdams1@users.noreply.github.com>
2020-12-18 20:51:58 +0000

    Add code (#34)
    * add_method w Adam, xent and softmax_prime
    
    * 80 per cent
    
    * works with multiple hidden layers
    
    * minor changes

--- unittest/unittest_neural_network.py
+++ unittest/unittest_neural_network.py
@@ -13,16 +13,16 @@ data = load_data.load_data()
 X = data['data']
 y = data['target']
 
-
 X_train, X_test, y_train, y_test = utils.split_data(X, y, 0.3)
 
 unittest_mod = nn.new_neural_network(0.001)
 unittest_mod.create_input_layer(784)
 unittest_mod.add_hidden_layer(256)
 #unittest_mod.add_hidden_layer(256)
-#unittest_mod.add_hidden_layer(128)
+unittest_mod.add_hidden_layer(128)
+#unittest_mod.add_hidden_layer(64)
 unittest_mod.add_output_layer(10)
-unittest_mod.new_train(X_train, y_train,3,batch_size = 32)
+unittest_mod.new_train(X_train, y_train,5,batch_size = 32, optimiser= "Adam")
 
 unittest_mod.accuracy_score(X_test, y_test)
 
