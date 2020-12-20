6bd0b23ecba9f11a65b5ba0e50f839691bc6aa50
Hugh Adams <hugh.adams@hotmail.com>
2020-12-20 12:33:23 +0000

    h

--- main.py
+++ main.py
@@ -4,18 +4,30 @@ Created on Tue Nov 17 15:23:18 2020
 
 @author: groes
 """
-import load_data
-
 import neural_network as nn
+import numpy as np
+import load_data
+import utils
 
-############ Calling the functions and class methods
-mnist_dataset = load_data.load_data()
+data = load_data.load_data()
+X = data['data']
+y = data['target']
+
+X_train, X_test, X_val, y_train, y_test, y_val = utils.split_data(X, y, 0.2)
 
 
-model = nn.Neural_network((100,100,50,10), (-0.1,-0.1,-0.1,0), 0.001)
-model.split_data(mnist_dataset.data, mnist_dataset.target, test_size = 0.2)
+unittest_mod = nn.new_neural_network(0.001)
+unittest_mod.create_input_layer(784)
+unittest_mod.add_hidden_layer(128)
+unittest_mod.add_hidden_layer(256)
+unittest_mod.add_hidden_layer(128)
+unittest_mod.add_output_layer(10)
+unittest_mod.new_train(X_train, X_val, y_train, y_val, 50 ,batch_size = 32, optimiser= "SGD", stopping_criterion= "xent")
 
-model.train(3)
 
-model.evaluate(model.X_test)
-model.accuracy(model.predict(model.X_test), model.y_test_onehot)
+unittest_mod.accuracy_score(X_test, y_test, cm = True)
+ 
+y_test[0]
+
+
+
