2809bc4dca26c93181fc5c354961ca2d95cc0c6f
HughAdams1 <73829484+HughAdams1@users.noreply.github.com>
2020-12-20 14:36:04 +0000

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -4,20 +4,18 @@ Created on Sat Dec 12 21:04:42 2020
 
 @author: hugha
 """
-
 import numpy as np
 import random
-
+#import utils
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
 
 class new_neural_network:
     def __init__(self, learning_rate):
-        #self.hidden_layers = hidden_layers
-        #self.hidden_plus_output = hidden_layers + [10]
         self.lr = learning_rate        
-        #self.bias = [np.ones(layer) for layer in self.hidden_plus_output]
-        
-        
         self.layers = []
+
 
     # Adding methods for creating layers to make it easier to create layers 
     # with different parameters
@@ -30,13 +28,11 @@ class new_neural_network:
             "bias" : bias,
             "number_of_neurons" : number_of_neurons,# making it parameterizable such that we can test the network on XOR
             "activations" : [],
-            "bias_update" : []
+            "bias_update" : [],
+            "stored_bias" : []
             }
         self.layers.append(parameters)
         
-
-    # Setting deafult activation func to relu as our experiments suggest Sigmiod  
-    # is performing poorly for the MNIST classification problem
     def add_hidden_layer(self, number_of_neurons, activation_func="relu", bias=0.0001):
         parameters = {
             "layer_type" : "hidden",
@@ -50,7 +46,9 @@ class new_neural_network:
             "r" : 0,
             "delta" : [],
             "weight_update" : [],
-            "bias_update" : []
+            "bias_update" : [],
+            "stored_weights" : [],
+            "stored_bias" : []
             }
         self.layers.append(parameters)
 
@@ -65,30 +63,30 @@ class new_neural_network:
             "s" : 0,
             "r" : 0,
             "delta" : [],
-            "weight_update" : []
+            "weight_update" : [],
+            "stored_weights" : [],
+            "stored_bias" : []
             }
         self.layers.append(parameters)
 
-    def initialize_weight_matrix(self, number_of_neurons, initialization_method="gaussian"):
-
-        # If we have time, we can experiment with different ways of initializing
-        if initialization_method == "xavier":
-            pass
-            # put return inside if statement so as to not run through the remaining 
-            # if statements if condition is fulfilled 
-        if initialization_method == "0":
-            pass
-            # put return inside if statement so as to not run through the remaining 
-            # if statements if condition is fulfilled 
-        #is this where bias goes?
+    def initialize_weight_matrix(self, number_of_neurons):
         return np.random.uniform(-1,1,(self.layers[-1]["number_of_neurons"],number_of_neurons))
         
         
-    def new_train(self, X, y, epochs, batch_size = 128, optimiser = "Adam"):
+    def new_train(self, X_train, X_val, y_train, y_val, epochs, batch_size = 128, optimiser = "Adam", tolerance = 0.01, max_patience = 10, stopping_criterion = "xent"):
         print("Training Started")
-        for epoch in range(epochs):
-            
-            mini_batches = self.get_minibatches(X,y, batch_size)           
+        stop_crit_prev = 1
+        patience = 0
+        epoch = 1
+        
+        val_xent_list = []
+        train_xent_list = []
+        val_accuracy_list = []
+        train_accuracy_list =[]
+        
+        #epochs and no improvement stopper
+        while epoch <= epochs and patience < max_patience:  
+            mini_batches = self.get_minibatches(X_train,y_train, batch_size)           
             for batch in mini_batches:
                 X_batch = [x for (x,y) in batch]
                 y_batch = [y for (x,y) in batch]
@@ -96,12 +94,81 @@ class new_neural_network:
                 self.forward_pass(X_batch)
                 #print("backward pass started")
                 self.backward_pass(y_batch, optimiser)
+ 
+            #calculate accuracy on a portion of training data  
+            train_batches = self.get_minibatches(X_train,y_train,round(len(X_train)*0.2) )
+            train_X_batch = [x for (x,y) in train_batches[0]]
+            train_y_batch = [y for (x,y) in train_batches[0]]
             
-            self.forward_pass(X)                
-            accuracy = self.accuracy_score(X, y)
-            xent = self.xent(self.layers[-1]['activations'], y)
-            print("Epoch {}: loss {}, accuracy = {}".format(epoch,xent, accuracy))
+            #validation set accuracy
+            val_accuracy = self.accuracy_score(X_val, y_val)
+            val_accuracy_list.append(val_accuracy)
+            
+            #training set accuracy
+            train_accuracy = self.accuracy_score(train_X_batch, train_y_batch)            
+            train_accuracy_list.append(train_accuracy)
+            
+            #validate set cross entropy
+            self.forward_pass(X_val)
+            val_xent = self.xent(self.layers[-1]['activations'], y_val)
+            val_xent_list.append(sum(val_xent)/len(val_xent))
+            #training set cross entropy
+            self.forward_pass(X_train)
+            train_xent = self.xent(self.layers[-1]['activations'], y_train)
+            train_xent_list.append(sum(train_xent)/len(train_xent))
 
+            
+            #gives option of stopping criterion
+            if stopping_criterion == "xent":
+                xent_mean = sum(val_xent)/len(val_xent)
+                stop_crit = xent_mean
+            elif stopping_criterion == "accuracy": 
+                stop_crit = val_accuracy
+            else:
+                print("xent or accuracy, you choose")  
+                
+            if abs(stop_crit - stop_crit_prev)<tolerance:
+                if patience == 0:
+                    #stores weights before they overfit
+                    for layer in self.layers:
+                        if layer['layer_type'] == "input":
+                            layer["stored_bias"] = layer["bias"]
+                        elif layer['layer_type'] == "output":
+                            layer["stored_weights"] = layer["weight_matrix"]
+                        else:
+                            layer["stored_bias"] = layer["bias"]
+                            layer["stored_weights"] = layer["weight_matrix"]
+                         
+                        
+                #returns matrices to their pre over-fitting peak           
+                if patience == max_patience-1:
+                    for layer in self.layers:
+                        if layer['layer_type'] == "input":
+                            layer["bias"] = layer["stored_bias"]
+                        elif layer['layer_type'] == "output":
+                            layer["weight_matrix"] = layer["stored_weights"]
+                        else:
+                            layer["bias"] = layer["stored_bias"]
+                            layer["weight_matrix"] = layer["stored_weights"]                                   
+                patience +=1
+            else:
+                patience = 0
+                             
+            stop_crit_prev = stop_crit 
+             
+            print("Epoch {}: loss {}, accuracy = {}".format(epoch,val_xent, val_accuracy))
+            epoch +=1
+        fig = plt.figure()
+        fig.add_subplot(2,2,1)
+        plt.plot(range(epoch-1), val_accuracy_list, label = 'validation')
+        plt.plot(range(epoch-1), train_accuracy_list, label = 'training')
+        plt.xlabel('Accuracy over epochs')
+        
+        fig.add_subplot(2,2,3)
+        plt.plot(range(epoch-1), val_xent_list, label = 'validation')
+        plt.plot(range(epoch-1), train_xent_list, label = 'training')
+        plt.xlabel('Cross Entropy over epochs')
+        fig.show()
         
     def get_minibatches(self, X, y, batch_size):
         training_data = [n for n in zip(X,y)]        
@@ -155,6 +222,8 @@ class new_neural_network:
                 self.layers[layer]['weight_matrix'] +=self.layers[layer]['weight_update']
                 self.layers[layer]['bias'] += self.layers[layer]['bias_update']
     
+    #################################################################
+
     def calculate_activations(self, prev_layer, current_layer):
         product = np.matmul(prev_layer['activations'], current_layer['weight_matrix']) + prev_layer['bias']
         
@@ -173,10 +242,6 @@ class new_neural_network:
         return np.maximum(0, x)
         
     def sigmoid_activation_func(self, x):
-        #x = x.float()
-        #x = np.exp(-x)
-        #x = x + 1
-        #x = 1/x
         sig = 1/(1 + np.exp(-x))
         return sig    
     
@@ -188,7 +253,9 @@ class new_neural_network:
         return np.array(new_array)
 
     
-    
+    #################################################################    
+
+    #Adam
     def Adam(self, current_layer, previous_layer, rho1 = 0.9, rho2 = 0.999, stab = 10e-8):
         gradient = self.SGD(current_layer, previous_layer)
         #I don't know why the t is here or what it does
@@ -202,24 +269,22 @@ class new_neural_network:
         update = s_hat / (r_hat ** 0.5 + stab)
         
         return update
-    
+    #Stochastic Gradient Descent
     def SGD(self, current_layer, prev_layer):
         weights = []
         for i in range(len(current_layer['delta'])):
             weights.append(current_layer['delta'][i] * prev_layer['activations'][i][:,np.newaxis])
         return sum(weights) / len(weights)
-
     
+    #backpropogates the cost to the layer-1
     def cost_prime_hidden(self, current_layer,layer_after):
         return np.array(self.activation_prime(current_layer)* np.matmul(layer_after['delta'], layer_after['weight_matrix'].transpose()))
 
-    
     def weight_update(self, err, activation):
         weights = []
         for i in range(len(activation)):
             weights.append(err[i] * activation[i][:,np.newaxis])
         return weights
-    
     
     def activation_prime(self, layer):
         if layer['activation_func'] == "sigmoid":
@@ -227,33 +292,48 @@ class new_neural_network:
         elif layer['activation_func'] == 'relu':
             return self.relu_prime(layer['g_inputs'])
 
-        
     def sigmoid_prime(self, g_input):
         return self.sigmoid_activation_func(g_input)*(1-self.sigmoid_activation_func(g_input)) 
-    
     
     def relu_prime(self, g_input):
         return np.where(g_input>0, 1.0, 0.0)  
     
-    
+    ####################################################################
+    #Check that these work for correct size dims
+    #Mean Squared Error
     def MSE(self, output_layer, y_batch):
         why = output_layer['activations'] - y_batch
         why_squared = why*why
         MSE = (1/len(y_batch))*np.sum(why_squared)
         return MSE
     
-    
+    #Cross Entropy
     def xent(self, output_layer, y_batch):
         return - sum(y_batch * np.log(output_layer))/len(y_batch)
     
-    def accuracy_score(self,X, y):
+    def accuracy_score(self,X, y, cm = False):
         self.forward_pass(X)
         y_predicted = self.layers[-1]['activations']
         accuracy_sum = 0
+        y_pred_arg = []
+        y_arg = []
         for datapoint in range(len(y)):
             if np.argmax(y[datapoint]) == np.argmax(y_predicted[datapoint]):
+                
+                
                 accuracy_sum += 1
+            y_pred_arg.append(np.argmax(y_predicted[datapoint]))
+            y_arg.append(np.argmax(y[datapoint]))
         
+        if cm == True:
+        
+            mat = confusion_matrix(y_pred_arg, y_arg)
+            sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False,
+                        xticklabels=range(10),
+                        yticklabels=range(10))
+            plt.xlabel('true label')
+            plt.ylabel('predicted label')
+            plt.show()
+
         accuracy = accuracy_sum/len(y)
         return accuracy*100
-    
